461137a410590b70138bb1c7b1f2978ac7838b53
LunaCodee <laura.zumbakiene@gmail.com>
2023-06-30 06:19:49 +0300

    registration

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,28 +4,8 @@ import axios from 'axios';
 import styles from './styles.module.css';
 import Navbar from "../components/Navbar/Navbar";
 import Footer from "../components/Footer/Footer";
-import RegisterForm from '../components/RegisterForm/RegisterForm';
 
 const HomePage = () => {
-  const [showRegisterForm, setShowRegisterForm] = useState(false);
-  const [registrationResponse, setRegistrationResponse] = useState(null);
-
-  const handleRegisterClick = () => {
-    setShowRegisterForm(true);
-  };
-
-  const handleRegistration = async (formData) => {
-    try {
-      const response = await axios.post('http://localhost:8081/register', formData);
-      setRegistrationResponse(response.data.response);
-    } catch (error) {
-      if (error.response) {
-        setRegistrationResponse(error.response.data.response);
-      } else {
-        setRegistrationResponse('An error occurred during registration.');
-      }
-    }
-  };
 
   return (
     <>
@@ -36,21 +16,13 @@ const HomePage = () => {
         <p className={styles.description}>Join the conversation and connect with others.</p>
       </div>
       <div className={styles.buttons}>
-        {/* <Link href="/register">
+        <Link href="/register">
           <span className={`${styles.button} ${styles.register}`}>Register</span>
         </Link>
         <Link href="/login">
           <span className={`${styles.button} ${styles.login}`}>Login</span>
-        </Link> */}
-         {showRegisterForm ? (
-            <RegisterForm onRegistration={handleRegistration} />
-          ) : (
-            <button className={`${styles.button} ${styles.register}`} onClick={handleRegisterClick}>
-              Register
-            </button>
-          )}
+        </Link>
       </div>
-      {registrationResponse && <p>{registrationResponse}</p>}
     </div>
     <Footer />
     </>
---
+++ src/pages/register/index.js
@@ -0,0 +1,92 @@
+import React, { useState } from "react";
+import styles from "./styles.module.css";
+import axios from "axios";
+import { useRouter } from "next/router";
+import Navbar from "../../components/Navbar/Navbar";
+import Footer from "../../components/Footer/Footer";
+
+const RegisterForm = () => {
+  const router = useRouter();
+
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [successMessage, setSuccessMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const addNewUser = async () => {
+    try {
+      const response = await axios.post(
+        "http://localhost:8081/register",
+        {
+          username: username,
+          email: email,
+          password: password,
+        }
+      );
+
+      console.log("response", response);
+      router.push("/");
+    } catch (err) {
+      console.log("err", err);
+    }
+  };
+
+  const handleNewUser = () => {
+    setSuccessMessage("Registration was successful!");
+    setTimeout(() => {
+      setSuccessMessage("");
+      router.push("/");
+    }, 5000);
+  };
+
+  const handleSubmit = () => {
+    if (username && email && password) {
+      addNewUser();
+      handleNewUser();
+    } else {
+      setErrorMessage("* Please fill in all the fields");
+      setTimeout(() => {
+        setErrorMessage("");
+      }, 1000);
+    }
+  };
+
+  return (
+    <>
+      <Navbar />
+      <div className={styles.container}>
+        <div className={styles.form}>
+          <h2>Registration form:</h2>
+
+          <input
+            placeholder="Username"
+            value={username}
+            onChange={(event) => setUsername(event.target.value)}
+          />
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+          <button onClick={handleSubmit}>Register</button>
+          {successMessage && <p>{successMessage}</p>}
+          {errorMessage && (
+            <p className={styles.errorMessage}>{errorMessage}</p>
+          )}
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default RegisterForm;

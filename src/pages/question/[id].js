1059faaf2be9a3da3a0f2e3f91d39fc3a054fea4
LunaCodee <laura.zumbakiene@gmail.com>
2023-07-01 22:36:28 +0300

    like/dislike buttons

--- src/pages/question/[id].js
+++ src/pages/question/[id].js
@@ -1,59 +1,3 @@
-// import React, { useEffect, useState } from "react";
-// import axios from "axios";
-// import Navbar from "../../components/Navbar/Navbar";
-// import Footer from "../../components/Footer/Footer";
-// import styles from "./styles.module.css";
-// import { useRouter } from "next/router";
-
-// const QuestionPage = () => {
-//   const router = useRouter();
-//   const { id } = router.query;
-
-//   const [question, setQuestion] = useState(null);
-//   const [answers, setAnswers] = useState([]);
-
-//   useEffect(() => {
-//     const fetchQuestion = async () => {
-//       try {
-//         const response = await axios.get(`http://localhost:8081/question/${id}`);
-//         const fetchedQuestion = response.data.question;
-//         setQuestion(fetchedQuestion);
-//         setAnswers(fetchedQuestion.answers_ids);
-//       } catch (error) {
-//         console.log("Error fetching question:", error);
-//       }
-//     };
-
-//     if (id) {
-//       fetchQuestion();
-//     }
-//   }, [id]);
-
-//   return (
-//     <>
-//       <Navbar />
-//       <div className={styles.container}>
-//         {question ? (
-//           <div className={styles.question}>
-//             <h1 className={styles.questionText}>{question.question_text}</h1>
-//             <h2>Answers:</h2>
-//             <ul>
-//               {question.answers_ids.map((answerId) => (
-//                 <li key={answerId}>{answerId}</li>
-//               ))}
-//             </ul>
-//           </div>
-//         ) : (
-//           <div>Loading...</div>
-//         )}
-//       </div>
-//       <Footer />
-//     </>
-//   );
-// };
-
-// export default QuestionPage;
-
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Navbar from "../../components/Navbar/Navbar";
@@ -124,38 +68,6 @@ const QuestionPage = () => {
       console.log('Error disliking answer:', error);
     }
   };
-
-//   return (
-//     <>
-//       <Navbar />
-//       <div className={styles.container}>
-//         <h1 className={styles.questionText}>{question?.question_text}</h1>
-//         <ul className={styles.answerList}>
-//           {answers.map((answer) => (
-//             <li key={answer._id} className={styles.answerItem}>
-//               {answer.answer_text} - Likes: {answer.gained_likes_number} - Dislikes: {answer.gained_dislikes_number}
-
-//               <button onClick={() => handleLike(answer._id)} className={styles.voteButton}>Like</button>
-//               <button onClick={() => handleDislike(answer._id)} className={styles.voteButton}>Dislike</button>
-//             </li>
-//           ))}
-//         </ul>
-//         <form onSubmit={handleNewAnswerSubmit}>
-//           <textarea
-//             value={newAnswer}
-//             onChange={handleNewAnswerChange}
-//             placeholder="Your answer..."
-//             className={styles.newAnswerInput}
-//           />
-//           <button type="submit" className={styles.submitButton}>
-//             Submit answer
-//           </button>
-//         </form>
-//       </div>
-//       <Footer />
-//     </>
-//   );
-// };
 
 return (
   <>
